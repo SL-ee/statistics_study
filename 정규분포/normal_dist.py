45f592bb1372a8a9247a0e77e7f1f03952b2e683
이수민(Soomin Lee) <71430607+SL-ee@users.noreply.github.com>
2021-08-25 17:53:03 +0900

    Create normal_dist.py

---
+++ 정규분포/normal_dist.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import erf
+
+np.random.seed(0)
+
+plt.style.use('default')
+# 그래프 창 크기
+plt.rcParams['figure.figsize'] = (10, 5)
+plt.rcParams['font.size'] = 12
+plt.rcParams['lines.linewidth'] = 5
+
+# 평균
+mean = 20.0
+# 표준편차
+deviation = 5.0
+
+x = np.linspace(0, 40, 1500)
+y = (1 / np.sqrt(2 * np.pi * deviation**2)) * np.exp(-1/2 * ((x - mean) / deviation)**2)
+
+plt.plot(x, y, alpha = 0.7, color="lightcoral",label="pdf of N(20,5)")
+plt.xlabel('x')
+plt.ylabel('f(x)')
+# 범례 위치
+plt.legend(loc="upper left")
+plt.show()
